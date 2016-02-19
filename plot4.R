878f64b6aa99aab5ca4723fa561d9437e2840516
Joon Kwon <joon.kwon@bellmedia.ca>
2016-02-19 15:40:14 -0500

    plot4 legend fixed

--- plot4.R
+++ plot4.R
@@ -19,12 +19,12 @@ with(power_s, {
     plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
     lines(datetime, Sub_metering_2, type="l", col="red" )
     lines(power_s$datetime, power_s$Sub_metering_3, type="l", col="blue")
-    legend("topright", pch="_", col=c("black", "red", "blue"), bty = "n",
-           legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+    legend("topright", lty=c(1,1), lwd=c(1,1),col=c("black", "red", "blue"), bty = "n",
+           cex=0.7, legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
     )
     plot(datetime, Voltage, type="l")
     plot(datetime, Global_reactive_power, type="l")
 })
 
-dev.copy(png, file="plot4.png")
+dev.copy(png, file="plot4.png", width = 480, height=480)
 dev.off()
